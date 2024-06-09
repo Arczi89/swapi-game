1e719042f0c4923a3625c3fee4906c99c9a32e35
Artur Szwagrzak <artur@szwagrzak.pl>
2024-06-09 16:57:40 +0200

    unit tests for select-category

--- src/app/select-category/select-category.component.spec.ts
+++ src/app/select-category/select-category.component.spec.ts
@@ -1,14 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SelectCategoryComponent } from './select-category.component';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { SwapiService } from '../swapi.service';
+import { By } from '@angular/platform-browser';
 
 describe('SelectCategoryComponent', () => {
   let component: SelectCategoryComponent;
   let fixture: ComponentFixture<SelectCategoryComponent>;
 
+  let mockRouter = { navigate: jasmine.createSpy('navigate') };
+  let mockSwapiService = {
+    setCategory: jasmine.createSpy('setCategory'),
+    setAttribute: jasmine.createSpy('setAttribute')
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SelectCategoryComponent]
+      imports: [SelectCategoryComponent, MatButtonModule, MatRadioModule, MatCardModule, FormsModule],
+      declarations: [],
+      providers: [
+        { provide: Router, useValue: mockRouter },
+        { provide: SwapiService, useValue: mockSwapiService }
+      ]
     })
     .compileComponents();
 
@@ -21,13 +39,57 @@ describe('SelectCategoryComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // blokowanie przycisku zanim dane będą wypełnione
   it('should pass to battle only with complete gathered informations', () => {
+    const button = fixture.debugElement.query(By.css('#battle-button')).nativeElement;
 
+    component.category = '';
+    fixture.detectChanges();
+    expect(button.disabled).toBeTruthy();
+
+    const categoryRadio = fixture.debugElement.query(By.css('#choose-category input[value="people"]')).nativeElement;
+    console.log(categoryRadio);
+    categoryRadio.click();
+    fixture.detectChanges();
+    expect(button.disabled).toBeTruthy();
+
+    const heightRadio = fixture.debugElement.query(By.css('#attribute-people input[value="height"]')).nativeElement;
+    console.log(heightRadio);
+    heightRadio.click();
+    fixture.detectChanges();
+    expect(button.disabled).toBeFalsy();
+
+    const starshipsRadio = fixture.debugElement.query(By.css('#choose-category input[value="starships"]')).nativeElement;
+    console.log(starshipsRadio);
+    starshipsRadio.click();
+    fixture.detectChanges();
+    expect(button.disabled).toBeTruthy();
+
+    const lengthRadio = fixture.debugElement.query(By.css('#attribute-starships input[value="length"]')).nativeElement;
+    console.log(lengthRadio);
+    lengthRadio.click();
+    fixture.detectChanges();
+    expect(button.disabled).toBeFalsy();
   });
 
-  // parametryzowany
-  it('should show only attributes connected to chosen category', () => {
+  [
+    ['people', ['height', 'mass']],
+    ['starships', ['cost_in_credits', 'length', 'passengers', 'cargo_capacity']]
+  ].forEach(([category, attributes]) => {
+    it(`should show only attributes connected to ${category} category`, () => {
+      component.category = category as string;
+      fixture.detectChanges();
 
+      const attrs = fixture.debugElement.query(By.css(`#attribute-${category}`)).children;
+      expect(attrs.length).toEqual(attributes.length);
+
+      (attributes as []).forEach((attribute: string) => {
+        const radioInputs = fixture.debugElement.queryAll(By.css(`#attribute-${category} input`)).map(input => input.nativeElement.value);
+        console.log(radioInputs);
+        console.log(attribute);
+        expect(radioInputs.includes(attribute)).toBeTrue;
+      });
+    });
   });
+
 });
+
--- src/app/select-category/select-category.component.ts
+++ src/app/select-category/select-category.component.ts
@@ -14,15 +14,28 @@ import { Router } from '@angular/router';
   styleUrl: './select-category.component.css'
 })
 export class SelectCategoryComponent {
-  category: any;
-  peopleAttribute: any;
-  starshipAttribute: any;
+  category = '';
+  peopleAttribute = '';
+  starshipsAttribute = '';
 
   constructor(protected router: Router, protected swapiService: SwapiService) {}
 
   battle() {
     this.swapiService.setCategory(this.category);
-    this.swapiService.setAttribute(this.category === 'people' ? this.peopleAttribute : this.starshipAttribute)
+    this.swapiService.setAttribute(this.category === 'people' ? this.peopleAttribute : this.starshipsAttribute)
     this.router.navigate(['/battle']);
+  }
+
+  isSelectionReady(): boolean {
+    return !!this.category && (!!this.peopleAttribute || !!this.starshipsAttribute)
+  }
+
+  onCategoryChange() {
+    this.clearAtributes();
+  }
+
+  private clearAtributes() {
+    this.peopleAttribute = '';
+    this.starshipsAttribute = '';
   }
 }
