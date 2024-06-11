ad9c64a74cdf2c82b9b93c7f16feae212e3140a6
Artur Szwagrzak <artur@szwagrzak.pl>
2024-06-11 19:20:41 +0200

    add e2e test in cypress

--- cypress/e2e/battle.cy.ts
+++ cypress/e2e/battle.cy.ts
@@ -1,5 +1,30 @@
 describe('Battle', () => {
+
+  beforeEach(() => {
+    cy.visit('/');
+    cy.get('#choose-category .category-option[value="people"]').click();
+    cy.get('.people-option input[value="height"]').click();
+    cy.get('#battle-button').click();
+  });
+
   it('page is loaded', () => {
-    cy.visit('/battle');
+    cy.url().should('contain','battle');
   })
+
+  it('should battle be resolved after PLAY button pressed', () => {
+
+    cy.intercept('GET', 'https://www.swapi.tech/api/people/*').as('getPeople');
+    cy.wait('@getPeople').its('response.statusCode').should('eq', 200);
+
+    cy.get('.card__item').should('have.length', 2);
+
+    cy.get('#battle-play').click();
+
+    cy.get('.counter .counter__opponent').invoke('text').then(values => {
+      expect(parseInt(values[0])).to.not.eq(parseInt(values[1]));
+    });
+
+    cy.get('#battle-play').should('contain', 'PLAY AGAIN');
+  });
+
 })
--- cypress/e2e/select-category.cy.ts
+++ cypress/e2e/select-category.cy.ts
@@ -1,5 +1,38 @@
 describe('Select category', () => {
-  it('page is loaded', () => {
+
+  beforeEach(() => {
     cy.visit('/');
-  })
+  });
+
+  it('should show people attributes when "People" category is selected', () => {
+    cy.get('#choose-category .category-option[value="people"]').click();
+    cy.get('#attribute-people').within(() => {
+      cy.get('.people-option').should('have.length', 2);
+      cy.get('.people-option label').eq(0).should('contain', 'Height');
+      cy.get('.people-option label').eq(1).should('contain', 'Mass');
+    });
+  });
+
+  it('should show starships attributes when "Starships" category is selected', () => {
+    cy.get('#choose-category .category-option[value="starships"]').click();
+    cy.get('#attribute-starships').within(() => {
+      cy.get('.starships-option').should('have.length', 5);
+      cy.get('.starships-option').eq(0).should('contain', 'Cost');
+      cy.get('.starships-option').eq(1).should('contain', 'Length');
+      cy.get('.starships-option').eq(2).should('contain', 'Passengers');
+      cy.get('.starships-option').eq(3).should('contain', 'Capacity');
+      cy.get('.starships-option').eq(4).should('contain', 'Crew');
+    });
+  });
+
+  it('Let`s go button should be enabled if category and attribute are selected', () => {
+    cy.get('#choose-category .category-option[value="people"]').click();
+    cy.get('.people-option input[value="height"]').click();
+    cy.get('#battle-button').should('not.be.disabled');
+  });
+
+  it('should display the header', () => {
+    cy.get('.header').should('contain', 'Choose a category and attributes');
+  });
+
 })
